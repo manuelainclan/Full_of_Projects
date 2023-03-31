56962f8f3cf07541adf227721c60b8f72f9ee653
marinacr92 <marinacaro92@gmail.com>
2023-03-31 14:37:16 +0200

    Update cardDetail

--- web/src/components/Preview/CardDetail.js
+++ web/src/components/Preview/CardDetail.js
@@ -1,55 +1,64 @@
-const CardDetail = ({ dataCardList }) => {
-  return dataCardList.map((obj, index) => {
-    return (
-      <div className="container">
-        <section key={index} className="landing-card">
-          <div
-            className="landing-card-background"
-            style={{
-              backgroundImage: `url(${obj.photo})`,
-            }}
-          ></div>
-          <button className="btn-remove-card" id={index} onClick="">
-            {/* <i className="fa-sharp fa-solid fa-circle-xmark icons"></i> */}
-            <i className="fa-solid fa-trash icons"></i>
-          </button>
-          <section className="project-info">
-            <p className="project-subtitle">Personal Project Card</p>
-            <hr className="landing-card-line" />
+import Header from '../Header';
+import logo from '../../images/logo-adalab.png';
+import logoFop from '../../images/logo-fop.png';
 
-            <h2 className="project-title">{obj.name}</h2>
-            <p className="project-slogan">{obj.slogan}</p>
-            <p className="project-desc">{obj.desc}</p>
-            <section className="tech-icons">
-              <section className="project-technologies">
-                <p className="text">{obj.technologies}</p>
+const CardDetail = ({ dataCardList }) => {
+  const renderCardDetail = () => {
+    return dataCardList.map((obj, index) => {
+      return (
+        <section className="preview">
+          <img className="preview-image" src={obj.photo} alt="" />
+          <section key={index} className="card">
+            <section className="project-info">
+              <p className="project-subtitle">Personal Project Card</p>
+              <hr className="landing-card-line" />
+
+              <h2 className="project-title">{obj.nameProject}</h2>
+              <p className="project-slogan">{obj.slogan}</p>
+              <p className="project-desc">{obj.descProject}</p>
+              <section className="tech-icons">
+                <section className="project-technologies">
+                  <p className="text">{obj.technologies}</p>
+                </section>
               </section>
             </section>
-          </section>
 
-          <section className="autor-info">
-            <img
-              className="autor-image"
-              src={obj.image}
-              alt="Foto de la autora"
-            />
-            <p className="autor-job">{obj.job}</p>
-            <p className="autor-name">{obj.autor}</p>
+            <section className="autor-info">
+              <img
+                className="autor-image"
+                src={obj.image}
+                alt="Foto de la autora"
+              />
+              <p className="autor-job">{obj.job}</p>
+              <p className="autor-name">{obj.autor}</p>
+            </section>
+            <button className="btn-fav" onClick="" id={index}>
+              {obj.isFavorite ? (
+                <i
+                  className="fa-solid fa-star icon-fav"
+                  style={{ color: '#fff700' }}
+                ></i>
+              ) : (
+                <i className="fa-regular fa-star icon-fav"></i>
+              )}
+            </button>
           </section>
-          <button className="btn-fav" onClick="" id={index}>
-            {obj.isFavorite ? (
-              <i
-                className="fa-solid fa-star icon-fav"
-                style={{ color: '#fff700' }}
-              ></i>
-            ) : (
-              <i className="fa-regular fa-star icon-fav"></i>
-            )}
-          </button>
         </section>
-      </div>
-    );
-  });
+      );
+    });
+  };
+
+  return (
+    <div className="container">
+      <Header
+        logoFop={logoFop}
+        logo={logo}
+        linkTo={'/'}
+        title="Volver a la página principal"
+      ></Header>
+      <main className="mainDetail">{renderCardDetail()}</main>
+    </div>
+  );
 };
 
 export default CardDetail;
