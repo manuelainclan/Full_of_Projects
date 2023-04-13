f3a2175441cd792c6e7a208c30fa9ead68d458f2
marinacr92 <marinacaro92@gmail.com>
2023-04-13 19:36:30 +0200

    Change conneccreate connection.

--- src/index.js
+++ src/index.js
@@ -19,36 +19,34 @@ server.listen(serverPort, () => {
 
 let connection;
 
-mysql
-  .createConnection({
-    host: 'sql.freedb.tech',
-    database: 'freedb_Full Of Projects',
-    user: 'freedb_Base64',
-    password: 'HFpUad@JqahhC6d',
-  })
-  .then((conn) => {
-    connection = conn;
-    connection
-      .connect()
-      .then(() => {
-        console.log(
-          `Conexión establecida con la base de datos (identificador=${connection.threadId})`
-        );
-      })
-      .catch((err) => {
-        console.error('Error de conexion: ' + err.stack);
-      });
-  })
-  .catch((err) => {
-    console.error('Error de configuración: ' + err.stack);
-  });
+function getConnection() {
+  return mysql
+    .createConnection({
+      host: 'sql.freedb.tech',
+      database: 'freedb_Full Of Projects',
+      user: 'freedb_Base64',
+      password: 'HFpUad@JqahhC6d',
+    })
+    .then((conn) => conn.connect())
+    .then((conn) => {
+      console.log(
+        `Conexión establecida con la base de datos (identificador=${connection.threadId})`
+      );
+      return conn;
+    })
+    .catch((err) => {
+      console.error('Error de configuración: ' + err.stack);
+    });
+}
 
 server.get('/api/projects/all', (req, res) => {
   console.log('Pidiendo a la base de datos información de las tarjetas.');
   let sql =
     'SELECT * FROM project, autor WHERE project.fkAutor = autor.idAutor;';
-  connection
-    .query(sql)
+  getConnection()
+    .then((connection) => {
+      return connection.query(sql);
+    })
     .then(([results, fields]) => {
       console.log('Información recuperada:');
       results.forEach((result) => {
